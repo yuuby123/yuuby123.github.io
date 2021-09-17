5a922d153679305d3fd6ae9c4b87461a7563535a
yuuby123 <79774539+yuuby123@users.noreply.github.com>
2021-09-17 16:48:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,24 +11,9 @@ if (url.searchParams.get('by') != null) {
 
 
 document.querySelector(".tombol").addEventListener('click', function () {
-  Swal.fire("Hallo Baby", "Aku ada pertanyaan nih buat kamu By", "question").then(function () {
-    Swal.fire("Jawab yang jujur ya!").then(function () {
-      Swal.fire("Awas aja kalau kamu bohong", "", "error").then(function () {
-
-        const {
-          value: name
-        } = Swal.fire({
-          title: 'Masukin nama kamu dulu',
-          input: 'text',
-          inputLabel: '',
-          showCancelButton: true,
-          inputValidator: (value) => {
-            if (!value) {
-              return 'Isi dulu dong by'
-            } else {
-              nama = value;
-            }
-          }
+  Swal.fire("Hallo sayang", "aku kangen nih:(", "question").then(function () {
+    Swal.fire("Boleh VC ga?").then(function () {
+     
         }).then(function () {
           const pertanyaan = Swal.fire({
             title: `${nama} sayang ga sama ${sender}?`,
